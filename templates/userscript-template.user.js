2f2c6e93f00a21fe7b615589d4b5730c08c54194
XXX2024XXX <freelife0freelife1+GitHub@gmail.com>
2026-08-05 16:11:51 +0900

    Add userscript template

---
+++ templates/userscript-template.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         新しいTampermonkeyスクリプト
+// @namespace    https://github.com/XXX2024XXX/tampermonkey-scripts
+// @version      1.0
+// @description  新しいTampermonkeyスクリプトのテンプレート
+// @match        https://example.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/REPLACE_FILENAME.user.js
+// @downloadURL  https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/REPLACE_FILENAME.user.js
+// ==/UserScript==
+
+(() => {
+    'use strict';
+})();
