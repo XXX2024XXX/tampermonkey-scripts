84d7f9a4b2fa36b51244d6f52a456373a6e6ea5a
XXX2024XXX <freelife0freelife1+GitHub@gmail.com>
2026-08-05 15:31:36 +0900

    Add sample userscript

---
+++ scripts/sample.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Sample Userscript
+// @namespace    https://github.com/XXX2024XXX/tampermonkey-scripts
+// @version      1.0
+// @description  Tampermonkey自動更新確認用のサンプルスクリプト
+// @match        https://example.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/sample.user.js
+// @downloadURL  https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/sample.user.js
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    console.log('[Sample Userscript] version 1.0');
+})();
