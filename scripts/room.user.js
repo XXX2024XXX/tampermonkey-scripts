d7188be612344095a9a254e5976ede989ceeab73
XXX2024XXX <freelife0freelife1+GitHub@gmail.com>
2026-08-05 15:31:48 +0900

    Add Rakuten ROOM userscript

---
+++ scripts/room.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         楽天ROOM Userscript
+// @namespace    https://github.com/XXX2024XXX/tampermonkey-scripts
+// @version      1.0
+// @description  楽天ROOM用Tampermonkeyスクリプトの管理ファイル
+// @match        https://room.rakuten.co.jp/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/room.user.js
+// @downloadURL  https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/room.user.js
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    console.log('[楽天ROOM Userscript] version 1.0');
+})();
