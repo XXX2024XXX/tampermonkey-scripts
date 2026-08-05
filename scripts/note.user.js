7196996da6c59fd69a309d0b0687a349093557e0
XXX2024XXX <freelife0freelife1+GitHub@gmail.com>
2026-08-05 15:32:02 +0900

    Add note userscript

---
+++ scripts/note.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         note Userscript
+// @namespace    https://github.com/XXX2024XXX/tampermonkey-scripts
+// @version      1.0
+// @description  note用Tampermonkeyスクリプトの管理ファイル
+// @match        https://note.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/note.user.js
+// @downloadURL  https://raw.githubusercontent.com/XXX2024XXX/tampermonkey-scripts/main/scripts/note.user.js
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    console.log('[note Userscript] version 1.0');
+})();
