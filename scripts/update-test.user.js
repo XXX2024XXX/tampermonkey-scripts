0059e3ce56fb54ddae8a4f458303da178979ad03
XXX2024XXX <freelife0freelife1+GitHub@gmail.com>
2026-08-05 18:14:59 +0900

    test: update Tampermonkey script to step 2

--- scripts/update-test.user.js
+++ scripts/update-test.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
-// @name         1
+// @name         2
 // @namespace    https://github.com/XXX2024XXX/tampermonkey-scripts
-// @version      1.1
+// @version      1.2
 // @description  Tampermonkeyの1クリック更新を10回確認するテスト用スクリプト
 // @author       XXX2024XXX
 // @match        https://example.com/*
@@ -18,7 +18,7 @@
 
     const panel = document.createElement('div');
     panel.id = 'tm-update-test-panel';
-    panel.textContent = 'Tampermonkey更新テスト：1回目';
+    panel.textContent = 'Tampermonkey更新テスト：2回目';
     Object.assign(panel.style, {
         position: 'fixed',
         top: '20px',
